1a8df096ae71837adbadb40dec758e349812f7ef
Arumpcp <Arumpradana12@gmail.com>
2025-03-12 02:15:56 +0700

    Integrasi leaflet draw dan modal point untuk membuat marker

---
+++ resources/views/table.blade.php
@@ -0,0 +1,36 @@
+@extends('layout.template')
+
+@section('content')
+<div class="container mt-4">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Kelas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr> 
+                <td>1</td>
+                <td>Arum Pradana</td>
+                <td>23/514595/SV/22387</td>
+                <td>B</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Nanda Amalia</td>
+                <td>-</td>
+                <td>B</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Amelia Nuraini</td>
+                <td>-</td>
+                <td>B</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+@endsection
